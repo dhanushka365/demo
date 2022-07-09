4e7f5810c1b6adb346a499722f0fd8bc3c4a66f8
dhanushka365 <technohub10000@gmail.com>
2022-07-09 08:45:29 +0530

    completed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,8 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
